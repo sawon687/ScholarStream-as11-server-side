17de1b61c6c85f583cc917f8122bd122014b57a4
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2025-12-14 18:30:23 +0600

    initial server setup without secrets

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const port =process.env.PORT|| 3000;
-const cors=require('cors')
+const port = process.env.PORT || 3000;
+const cors = require('cors')
 app.use(cors())
 app.use(express.json())
 require('dotenv').config()
@@ -12,8 +12,8 @@ app.get('/', (req, res) => {
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri =`mongodb+srv://${process.env.MDB_NAME}:${process.env.MDB_PASS}@cluster0.ofja8we.mongodb.net/?appName=Cluster0`;
-
+const uri = `mongodb+srv://${process.env.MDB_NAME}:${process.env.MDB_PASS}@cluster0.ofja8we.mongodb.net/?appName=Cluster0`;
+const stripe = require('stripe')(`${process.env.STRIPE_API_KEY}`);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -28,76 +28,159 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const db=client.db('scholarshipDB');
-    const scholarshipsColl=db.collection('scholarshipsColl');
-    const userColl=db.collection('userColl');
+    const db = client.db('scholarshipDB');
+    const scholarshipsColl = db.collection('scholarshipsColl');
+    const userColl = db.collection('userColl');
+    const applicationsColl = db.collection('applicationsCollection')
     // post scholarshipsColl
-    app.post('/scholarships',async(req,res)=>{
-         const scholarshipInfo=req.body;
-          scholarshipInfo.scholarshipPostDate=new Date()
-        
-       const result= await scholarshipsColl.insertOne(scholarshipInfo);
-       res.send(result)
+    app.post('/scholarships', async (req, res) => {
+      const scholarshipInfo = req.body;
+      scholarshipInfo.scholarshipPostDate = new Date()
+
+      const result = await scholarshipsColl.insertOne(scholarshipInfo);
+      res.send(result)
     })
     // scholarships get data
-    app.get('/scholarships',async(req,res)=>{
-        const  result=await scholarshipsColl.find().toArray()
-        res.send(result);
+    app.get('/scholarships', async (req, res) => {
+      const query = { scholarshipPostDate: "-1" }
+      const result = await scholarshipsColl.find().sort(query).toArray()
+      res.send(result);
     })
     //  singe scholarshps id
-    app.get('/scholarships/:id',async(req,res)=>{
-        const  id=req.params.id
-        const query={_id: new ObjectId(id)}
-        const  result=await scholarshipsColl.findOne(query)
-        res.send(result);
+    app.get('/scholarships/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+
+      const result = await scholarshipsColl.findOne(query)
+      res.send(result);
     })
-    app.post('/user',async(req,res)=>{
-          const userInfo=req.body;
-          userInfo.role="student";
-          userInfo.createAT=new Date()
-          const  query={email: userInfo.email}
-      
-          const  alreadyUser=await userColl.findOne(query)
-          if(alreadyUser)
-          {
-             return
-          }
-          
-          const result=await userColl.insertOne(userInfo)
-          res.send(result)
+    app.post('/user', async (req, res) => {
+      const userInfo = req.body;
+      userInfo.role = "student";
+      userInfo.createAT = new Date()
+      const query = { email: userInfo.email }
+
+      const alreadyUser = await userColl.findOne(query)
+      if (alreadyUser) {
+        return
+      }
+
+      const result = await userColl.insertOne(userInfo)
+      res.send(result)
     })
     // user get
 
-    app.get('/user',async(req,res)=>{
-        const role=req.query.role
-        const query={role}
-       
-        const result=await userColl.find(query).toArray();
-        res.send(result);
+    app.get('/user', async (req, res) => {
+      const role = req.query.role
+      const query = { role }
+
+      const result = await userColl.find(query).toArray();
+      res.send(result);
     })
 
-    app.patch('/user/:id',async(req,res)=>{
-        const id=req.params.id;
-        const query={_id: new ObjectId(id)}
-        const  updateinfo=req.body
-            const  update={
-            $set:{
-               role:updateinfo.role
-            }
-          }
-          const result =await userColl.updateOne(query,update)
+    app.patch('/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const updateinfo = req.body
+      const update = {
+        $set: {
+          role: updateinfo.role
+        }
+      }
+      const result = await scholarshipsColl.updateOne(query, update)
 
-          res.send(result)
-         
+      res.send(result)
+
     })
 
-    app.delete('/user/:id',async(req,res)=>{
-        const id=req.params.id;
-        const query={_id: new ObjectId(id)};
-        const result=await userColl.deleteOne(query)
-        res.send(result)
+    app.patch('/user/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+
+      const updateinfo = req.body
+      const update = {
+        $set: {
+          role: updateinfo.role
+        }
+      }
+      const result = await userColl.updateOne(query, update)
+
+      res.send(result)
+
     })
-   
+
+    app.patch('/scholarships/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateScholar = req.body
+      const query = { _id: new ObjectId(id) };
+
+      updateScholar.scholarshipPostDate = new Date()
+      const update = {
+        $set: {
+          updateScholar
+        }
+      }
+      const result = await scholarshipsColl.updateOne(query, update)
+      res.send(result)
+    })
+
+    app.delete('/scholarships/:id', async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+
+
+      const result = await scholarshipsColl.deleteOne(query)
+      res.send(result)
+    })
+//  payment api
+  app.post('/create-checkout-session', async (req, res) => {
+  try {
+    const applicationInfo = req.body;
+  
+    const session = await stripe.checkout.sessions.create({
+      customer_email: applicationInfo.userEmail,
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: 'USD',
+            product_data: {
+              name: applicationInfo.scholarshipName,
+              images: [applicationInfo.scholarImage],
+            },
+            unit_amount: applicationInfo.totalAmount * 100,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${process.env.SITE_DOMAIN}/payment-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.SITE_DOMAIN}/payment-failed`,
+      metadata: {
+        scholarshipId: applicationInfo.scholarshipId,
+        scholarshipName: applicationInfo.scholarshipName,
+        userEmail: applicationInfo.userEmail,
+        applicationFee: applicationInfo.applicationFee,
+        serviceCharge: applicationInfo.serviceCharge,
+        totalAmount: applicationInfo.totalAmount,
+      },
+    });
+
+    console.log('Stripe session created:', session.id);
+    res.json({ url: session.url });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// payment check
+
+app.patch('/payment-seccess',async(req,res)=>{
+  const sessionID=req.query.session_id
+  console.log(sessionID)
+})
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
