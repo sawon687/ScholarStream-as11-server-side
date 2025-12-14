f643c3cdeb1ac0d720d22ed81c49287702fa2ed6
Md AL JIHAD SAWON <islamsawon367@gmail.com>
2025-12-14 18:32:42 +0600

    added to payment method setup and checkout create

--- index.js
+++ index.js
@@ -177,10 +177,7 @@ async function run() {
 
 // payment check
 
-app.patch('/payment-seccess',async(req,res)=>{
-  const sessionID=req.query.session_id
-  console.log(sessionID)
-})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
